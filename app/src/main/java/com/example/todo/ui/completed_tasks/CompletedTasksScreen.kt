a10c58dbdd2616c062e1cd161736051cb0883181
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-01 13:47:21 +0900

    タスク表示画面のベースを追加

---
+++ app/src/main/java/com/example/todo/ui/completed_tasks/CompletedTasksScreen.kt
@@ -0,0 +1,9 @@
+package com.example.todo.ui.completed_tasks
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CompletedTasksScreen() {
+    Text(text = "CompletedTasksScreen")
+}
---
+++ app/src/main/java/com/example/todo/ui/incomplete_tasks/IncompletedTasks.kt
@@ -0,0 +1,9 @@
+package com.example.todo.ui.incomplete_tasks
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun IncompleteTasksScreen() {
+    Text(text = "IncompleteTasksScreen")
+}

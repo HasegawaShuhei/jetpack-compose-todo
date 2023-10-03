db8e6782213c3173fe2ffb79b35d0ff97d8bc237
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-03 21:31:55 +0900

    不要な記述削除

--- app/src/main/java/com/example/todo/ui/incomplete_tasks/IncompletedTasksScreen.kt
+++ app/src/main/java/com/example/todo/ui/incomplete_tasks/IncompletedTasksScreen.kt
@@ -1,7 +1,5 @@
 package com.example.todo.ui.incomplete_tasks
 
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -9,7 +7,4 @@ import androidx.hilt.navigation.compose.hiltViewModel
 fun IncompleteTasksScreen(
     viewModel: IncompleteTasksViewModel= hiltViewModel(),
 ) {
-    ElevatedButton(onClick = { viewModel.createTask() }) {
-        Text(text = "create")
-    }
 }

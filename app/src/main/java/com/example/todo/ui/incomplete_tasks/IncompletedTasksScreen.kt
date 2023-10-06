4fa5ec89e1959889a253c7c0e1ae806be7854669
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-06 22:36:03 +0900

    不要なインポート削除

--- app/src/main/java/com/example/todo/ui/incomplete_tasks/IncompletedTasksScreen.kt
+++ app/src/main/java/com/example/todo/ui/incomplete_tasks/IncompletedTasksScreen.kt
@@ -1,7 +1,6 @@
 package com.example.todo.ui.incomplete_tasks
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.todo.data.Task
 import com.example.todo.ui.components.TaskList
 
